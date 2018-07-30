4a007128a81634404a8ce68fe1195cc601997bb4
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2018-07-30 12:27:51 -0700

    fixed bug

--- grid-world.py
+++ grid-world.py
@@ -33,7 +33,9 @@ def _getNeighborsD(grid, distance):
 
 ''' Check if there is a neighboring point of some distance '''
 def _checkDistance(neighbors, distance, grid):
-	return np.any(grid[neighbors.T] == distance)
+	if len(neighbors):
+		return np.any(grid[np.array(neighbors)] == distance)
+	return False
 
 ''' Get a list of valid neighbors for any given point in the grid
 for the wavefront potential calculation '''
@@ -57,7 +59,7 @@ def _getNeighbors(point, grid):
 
 ''' Determine if a coordinate is out of bounds in a grid '''
 def _out_of_bounds(row, col, num_rows, num_cols):
-	return 0 <= rows < num_rows and 0 <= col <= num_cols
+	return not (0 <= row < num_rows and 0 <= col < num_cols)
 
 ''' Class to handle a 2D world representation where a set of agents navigate
 from starting positions to goal positions amidst a number of randomly
@@ -170,25 +172,22 @@ class GridWorld2D:
 
 		# Get row and column ranges for the neighborhood state grid
 		# and initialize the neighborhood grid.
-		rows = (self.agent_locs[agent_index][0] - int(n/2), self.agent_locs[agent_index][0] + int(n/2))
-		cols = (self.agent_locs[agent_index][1] - int(n/2), self.agent_locs[agent_index][1] + int(n/2))
-		neighborhood_grid = np.ones((n, n)) * WORLD['OBSTACLE']
+		rows = (self.agent_locs[agent_index][0] - int(n/2), self.agent_locs[agent_index][0] + int(n/2)  )
+		cols = (self.agent_locs[agent_index][1] - int(n/2), self.agent_locs[agent_index][1] + int(n/2)  )
+		neighborhood_grid = np.zeros((n, n))
 
 		# Fill in neighborhood grid from spaces in the world around the robot
 		# location.
-		# for row in range(rows[0], rows[1] + 1):
-		# 	for col in range(cols[0], cols[1] + 1):
-		# 		if _out_of_bounds(row, col, len(self.world), len(self.world[0])) == False:
-		# 			neighborhood_grid[row - rows[0], col - cols[0]] = self.world[row][col]
-		# TODO: Check this math
-		left = max(0, cols[0])
-		right = min(len(self.world[0]), cols[1] + 1)
-		top = max(0, rows[0])
-		bottom = min(len(self.world), rows[1] + 1)
-		offset = top - rows[0], left - cols[0]
-		local_grid = self.world[left:right, top:bottom]
-		localy, localx = local_grid.shape
-		neighborhood_grid[offset[0]:offset[0] + localy, offset[1]:offset[1]+localx]
+		for row in range(rows[0], rows[1] + 1):
+			for col in range(cols[0], cols[1] + 1):
+				if _out_of_bounds(row, col, len(self.world),
+				len(self.world[0])  ) == False:
+					neighborhood_grid[ row - rows[0],
+					col - cols[0] ] = self.world[row][col]
+				else:
+					neighborhood_grid[ row - rows[0],
+					col - cols[0] ] = WORLD['OBSTACLE']
+
 		return neighborhood_grid
 
 	''' Returns current world representation '''
